d57c3a027f018377e29f08652d46e379d1a34c4a
melsakelaris <106757766+melsakelaris@users.noreply.github.com>
2022-06-06 22:32:45 -0400

    exercise 1.6 6/6/22

---
+++ stackedlabels/stackedlabels/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  stackedlabels
+//
+//  Created by Consultant on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
